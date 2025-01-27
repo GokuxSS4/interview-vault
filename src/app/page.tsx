f9cbdde3bbd4348ffbef18f61e619003a40d2ab9
jaykumar286 <jaykumarchavda2806@gmail.com>
2025-01-27 15:57:45 +0530

    added an navbar with theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,7 @@ import { SignInButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="m-10">
-      <SignInButton>
-        <Button>
-          Sign in
-        </Button>
-      </SignInButton>
-
+     Home
     </div>
   );
 }
---
+++ src/components/DashboardBtn.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { SparklesIcon } from "lucide-react";
+
+function DasboardBtn() {
+
+//   if (isCandidate || isLoading) return null;
+
+  return (
+    <Link href={"/dashboard"}>
+      <Button className="gap-2 font-medium" size={"sm"}>
+        <SparklesIcon className="size-4" />
+        Dashboard
+      </Button>
+    </Link>
+  );
+}
+export default DasboardBtn;
